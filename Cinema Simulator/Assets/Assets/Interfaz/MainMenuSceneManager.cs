bd795ac52fc9a4b6adc6e4321873b78b3270c040
Hugo Juan Gomez <hugojuan09082004@gmail.com>
2025-11-29 22:25:52 +0100

    interfaz menu

---
+++ Cinema Simulator/Assets/Assets/Interfaz/MainMenuSceneManager.cs
@@ -0,0 +1,46 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuSceneManager : MonoBehaviour
+{
+
+    public Button defaultButton;
+
+    void Start()
+    {
+        EventSystem.current.SetSelectedGameObject(defaultButton.gameObject);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("03LevelOne");
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+
+    public void OnEnterName(string playerName)
+    {
+        Debug.Log("PlayerName: " + playerName);
+    }
+
+    public void OnSFXVolume(float volume)
+    {
+        Debug.Log("SFX Volume: " + volume);
+    }
+
+    public void OnMusicVolume(float volume)
+    {
+        Debug.Log("Music Volume: " + volume);
+    }
+}
